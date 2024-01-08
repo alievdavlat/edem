ff1a9f27cee11e976f505f1c51cb1bd36d5afe20
aliev <122043094+alievdavlat@users.noreply.github.com>
2024-01-08 17:51:12 +0500

    template added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,11 +4,7 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>
-      <h1 className='header'>
-        hello world
-      </h1>
-    </div>
+    <div>home</div>
   )
 }
 
